83b5a181aa5e80443f66229de516f1b26b9bb0b1
Marie Langer <98027776+marielngr@users.noreply.github.com>
2024-02-12 13:51:32 +0100

    add redirecting to detailspage after form submit

--- components/ProductForm/index.js
+++ components/ProductForm/index.js
@@ -1,26 +1,39 @@
 import { StyledForm, StyledHeading, StyledLabel } from "./ProductForm.styled";
 import { StyledButton } from "../Button/Button.styled";
 import { mutate } from "swr";
+import { useRouter } from "next/router";
 
 export default function ProductForm() {
+  const router = useRouter();
+
   async function handleSubmit(event) {
     event.preventDefault();
 
     const formData = new FormData(event.target);
     const productData = Object.fromEntries(formData);
 
+    //Browser API fetch für einen HTTP-request
     const response = await fetch("/api/products", {
       method: "POST",
+      //Browser sagt Server, dass der im body enthaltene Inhalt vom content-Type JSON ist (statt XML...)
       headers: {
         "Content-Type": "application/json",
       },
+      //http kennt nur Zeichenketten, daher wird das productData-Object in eine Zeichenkette umgewandelt
       body: JSON.stringify(productData),
     });
-    event.target.elements.name.focus();
-    event.target.reset();
 
+    // event.target.elements.name.focus();
+    // event.target.reset();
+
+    //swr cached, mit mutate sagt man dem Server, dass sich was geändert hat und er neu abrufen soll
     if (response.ok) {
       mutate("/api/products");
+
+      //redirecting nach submit zur neuen Detailspage
+      const product = await response.json();
+      console.log(product);
+      router.push(`/${product._id}`);
     }
   }
 
